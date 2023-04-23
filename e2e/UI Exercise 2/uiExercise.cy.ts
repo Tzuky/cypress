2b5726e72048f48add7f77bc5bc22be3fef6d46d
Tzuky <tzuky_@outlook.com>
2023-04-23 18:53:10 +0300

    some work on Orange UI test

--- e2e/UI Exercise 2/uiExercise.cy.ts
+++ e2e/UI Exercise 2/uiExercise.cy.ts
@@ -6,9 +6,26 @@ describe('Orange demo site tests', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/')
     })
 
-    it('Login', () => {
+    it('Login and logout', () => {
         cy.get('[name=username]').type('Admin')
         cy.get('[type=password]').type('admin123')
         cy.get('.oxd-button').click()
+        cy.get('.oxd-userdropdown-img').click()
+        cy.get('.oxd-userdropdown-link').contains('Logout').click()
+        cy.get('.oxd-text--h5').should('contain', 'Login')
+    })
+        
+    it ('Wrong Username - Fail Login', () => {
+        cy.get('[name=username]').type('Admin-Wrong')
+        cy.get('[type=password]').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-alert-content > .oxd-text').should('contain', 'Invalid credentials')
+    })
+
+    it ('Wrong Password - Fail Login', () => {
+        cy.get('[name=username]').type('Admin')
+        cy.get('[type=password]').type('admin123-4')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-alert-content > .oxd-text').should('contain', 'Invalid credentials')
     })
 })
